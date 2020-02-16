931c7b7c73b2c9d70892c895a90e3b9b961c273e
Roberto Luiz Veiga Junior <p000robejunior@prservicos.com.br>
2020-02-16 18:02:56 -0300

    fix config

--- setup/ConfigureSwift.rb
+++ setup/ConfigureSwift.rb
@@ -12,37 +12,37 @@ module Pod
     end
 
     def perform
-      keep_demo = configurator.ask_with_answers("Would you like to include a demo application with your library", ["Yes", "No"]).to_sym
+      keep_demo = configurator.ask_with_answers("Você gostaria de adicionar um template no seu Pod", ["S", "N"]).to_sym
 
-      framework = configurator.ask_with_answers("Which testing frameworks will you use", ["Quick", "None"]).to_sym
-      case framework
-        when :quick
-          configurator.add_pod_to_podfile "Quick', '~> 1.2.0"
-          configurator.add_pod_to_podfile "Nimble', '~> 7.0"
-          configurator.set_test_framework "quick", "swift", "swift"
+      # framework = configurator.ask_with_answers("Which testing frameworks will you use", ["Quick", "None"]).to_sym
+      # case framework
+      #   when :quick
+      #     configurator.add_pod_to_podfile "Quick', '~> 1.2.0"
+      #     configurator.add_pod_to_podfile "Nimble', '~> 7.0"
+      #     configurator.set_test_framework "quick", "swift", "swift"
+      #
+      #   when :none
+      #     configurator.set_test_framework "xctest", "swift", "swift"
+      # end
 
-        when :none
-          configurator.set_test_framework "xctest", "swift", "swift"
-      end
-
-      snapshots = configurator.ask_with_answers("Would you like to do view based testing", ["Yes", "No"]).to_sym
-      case snapshots
-        when :yes
-          configurator.add_pod_to_podfile "FBSnapshotTestCase' , '~> 2.1.4"
-
-          if keep_demo == :no
-              puts " Putting demo application back in, you cannot do view tests without a host application."
-              keep_demo = :yes
-          end
-
-          if framework == :quick
-              configurator.add_pod_to_podfile "Nimble-Snapshots' , '~> 6.3.0"
-          end
-      end
+      # snapshots = configurator.ask_with_answers("Would you like to do view based testing", ["Yes", "No"]).to_sym
+      # case snapshots
+      #   when :yes
+      #     configurator.add_pod_to_podfile "FBSnapshotTestCase' , '~> 2.1.4"
+      #
+      #     if keep_demo == :no
+      #         puts " Putting demo application back in, you cannot do view tests without a host application."
+      #         keep_demo = :yes
+      #     end
+      #
+      #     if framework == :quick
+      #         configurator.add_pod_to_podfile "Nimble-Snapshots' , '~> 6.3.0"
+      #     end
+      # end
 
       Pod::ProjectManipulator.new({
         :configurator => @configurator,
-        :xcodeproj_path => "templates/swift/Example/PROJECT.xcodeproj",
+        :xcodeproj_path => "templates/swift/Example/*.xcodeproj",
         :platform => :ios,
         :remove_demo_project => (keep_demo == :no),
         :prefix => ""
--- setup/TemplateConfigurator.rb
+++ setup/TemplateConfigurator.rb
@@ -69,22 +69,8 @@ module Pod
 
     def run
       @message_bank.welcome_message
-
-      platform = self.ask_with_answers("What platform do you want to use?", ["iOS", "macOS"]).to_sym
-
-      case platform
-        when :macos
-          ConfigureMacOSSwift.perform(configurator: self)
-        when :ios
-          framework = self.ask_with_answers("What language do you want to use?", ["Swift", "ObjC"]).to_sym
-          case framework
-            when :swift
-              ConfigureSwift.perform(configurator: self)
-
-            when :objc
-              ConfigureIOS.perform(configurator: self)
-          end
-      end
+      framework = self.ask_with_answers("What language do you want to use?", ["Swift", "ObjC"]).to_sym
+      ConfigureSwift.perform(configurator: self)
 
       replace_variables_in_files
       clean_template_files

b79c58961cdc399da67d4a9f613c9a0faa297126
Roberto Luiz Veiga Junior <p000robejunior@prservicos.com.br>
2020-02-17 15:06:45 -0300

    fix config

---
+++ templates/mvvmc/AfterUsingCoordinator/AppDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  AppDelegate.swift
+//  AfterUsingCoordinator
+//
+//  Created by Marcello Chuahy on 02/02/20.
+//  Copyright © 2020 Applause Codes. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    var coordinator: Coordinator001?
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+    {
+        setup()
+        return true
+    }
+    
+}
+
+extension AppDelegate {
+    
+    func setup() {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UINavigationController()
+        coordinator = Coordinator001(navigationController: window?.rootViewController as! UINavigationController)
+        coordinator?.makeRootViewController()
+        window?.makeKeyAndVisible()
+        
+    }
+}
+
+

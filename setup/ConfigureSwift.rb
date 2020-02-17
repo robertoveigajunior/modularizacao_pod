95d7a63c08ba10201109a7c4930d7852f5897798
Roberto Luiz Veiga Junior <p000robejunior@prservicos.com.br>
2020-02-17 14:49:40 -0300

    fix config

--- setup/ConfigureSwift.rb
+++ setup/ConfigureSwift.rb
@@ -12,33 +12,9 @@ module Pod
     end
 
     def perform
-      keep_demo = configurator.ask_with_answers("Would you like to include a demo application with your library", ["Yes", "No"]).to_sym
+      keep_demo = configurator.ask_with_answers("Você gostaria de adicionar um template em sua biblioteca", ["Sim", "Não"]).to_sym
 
-      framework = configurator.ask_with_answers("Which testing frameworks will you use", ["Quick", "None"]).to_sym
-      case framework
-        when :quick
-          configurator.add_pod_to_podfile "Quick', '~> 1.2.0"
-          configurator.add_pod_to_podfile "Nimble', '~> 7.0"
-          configurator.set_test_framework "quick", "swift", "swift"
-
-        when :none
-          configurator.set_test_framework "xctest", "swift", "swift"
-      end
-
-      snapshots = configurator.ask_with_answers("Would you like to do view based testing", ["Yes", "No"]).to_sym
-      case snapshots
-        when :yes
-          configurator.add_pod_to_podfile "FBSnapshotTestCase' , '~> 2.1.4"
-
-          if keep_demo == :no
-              puts " Putting demo application back in, you cannot do view tests without a host application."
-              keep_demo = :yes
-          end
-
-          if framework == :quick
-              configurator.add_pod_to_podfile "Nimble-Snapshots' , '~> 6.3.0"
-          end
-      end
+      configurator.set_test_framework "xctest", "swift", "swift"
 
       Pod::ProjectManipulator.new({
         :configurator => @configurator,
@@ -49,10 +25,7 @@ module Pod
       }).run
 
       `mv ./templates/swift/* ./`
-
-      # There has to be a single file in the Classes dir
-      # or a framework won't be created
-      `touch Pod/Classes/ReplaceMe.swift`
+      `mv ./templates/mvvmc Pod/Classes/`
 
       # The Podspec should be 8.0 instead of 7.0
       text = File.read("NAME.podspec")
